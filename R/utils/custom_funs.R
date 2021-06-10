c2f7c541bcfaffc0765af50808aa75a7005fca51
Phileas Condemine <phileas.condemine@sante.gouv.fr>
2021-06-10 10:33:35 +0200

    fix 2 steps GLM | fix encoding as a reusable fun | AUC can't run (n_pos x n_neg) is too big

--- R/utils/custom_funs.R
+++ R/utils/custom_funs.R
@@ -25,7 +25,20 @@ plot_margin = function(coeffs,pattern,signif=0.01){
   ggplotly(g)
 }
 
-two_step_glm = function(dt,target){
+
+fix_names_encoding = function(dt){
+  nm = names(dt)
+  to_fix = nm[!validUTF8(nm)]
+  to_fix <- iconv(to_fix,to="UTF-8")
+  names(dt)[!validUTF8(nm)] <- iconv(to_fix,from="UTF-8",to="ASCII//TRANSLIT")
+  names(dt)[validUTF8(nm)] <- iconv(names(dt)[validUTF8(nm)],from="UTF-8",to="ASCII//TRANSLIT")
+  names(dt) <- gsub(" ","_",names(dt))
+  names(dt) <- gsub("'",".",names(dt))
+  dt
+}
+
+
+two_step_glm = function(dt,target,output="coeffs"){
   dt = dt[,sapply(dt,function(x)uniqueN(na.omit(x)))>1,with=F]
   dt = dt%>%mutate_if(is.logical,function(x){
     case_when(is.na(x) ~ F,
@@ -39,13 +52,7 @@ two_step_glm = function(dt,target){
     case_when(is.na(x) ~ 0,
               T ~ x)
   })
-  nm = names(dt)
-  to_fix = nm[!validUTF8(nm)]
-  to_fix <- iconv(to_fix,to="UTF-8")
-  names(dt)[!validUTF8(nm)] <- iconv(to_fix,from="UTF-8",to="ASCII//TRANSLIT")
-  names(dt)[validUTF8(nm)] <- iconv(names(dt)[validUTF8(nm)],from="UTF-8",to="ASCII//TRANSLIT")
-  names(dt) <- gsub(" ","_",names(dt))
-  names(dt) <- gsub("'",".",names(dt))
+  dt <- fix_names_encoding(dt)
   setnames(dt,target,"target")
   model <- glm(target~.+0,data=dt)
   summary(model)
@@ -55,10 +62,45 @@ two_step_glm = function(dt,target){
     data.table(var=var,mod=unique(dt[[var]]))
   }))
   var_mod[,coeff:=paste0(var,mod)]
-  vars_signif = unique(var_mod[coeff%in%vars]$var)
+  vars_signif = unique(var_mod[coeff%in%vars | var%in%vars]$var)
   model <- glm(target~.+0,data=dt[,c(vars_signif,"target"),with=F])
   summary(model)
   coeffs <- summary(model)$coefficients
   coeffs = data.table(coeffs,keep.rownames = "var")
-  coeffs
+  if(output=="coeffs"){
+    return(coeffs)
+  } else if (output=="model"){
+    return(model)
+  } else {
+    stop("wrong output value, should be coeffs or model")
+  }
+}
+
+add_target_encoding = function(train,test,var,min_size=100,alias){
+  assertthat::assert_that(var %in% names(train),msg=sprintf('"%s" should be in train',var))
+  assertthat::assert_that(var %in% names(test),msg=sprintf('"%s" should be in test',var))
+  target_encoding = train[, .(
+    N=.N,
+    avg_access_hm = mean(OK_PRATIQUE_HM, na.rm = T), #target encoding
+    avg_access_hs = mean(OK_PRATIQUE_HS, na.rm = T) #target encoding
+  ), by = var]
+  setnames(target_encoding,c("avg_access_hm","avg_access_hs"),
+           c(paste0(alias,"_avg_access_hm"),paste0(alias,"_avg_access_hs")))
+  n=nrow(target_encoding)
+  target_encoding = target_encoding[N>min_size,-c("N")]
+  n2=nrow(target_encoding)
+  print(sprintf("Avec le filtrage sur min_size on passe de %s modalités à %s",n,n2))
+  train = merge(train,target_encoding,by=var,all.x=T)
+  test = merge(test,target_encoding,by=var,all.x=T)
+  list("train"=train,"test"=test)
+}
+
+AUC2 = function (y_pred, y_true) {
+  rank <- rank(y_pred)
+  n_pos <- sum(y_true == 1)
+  n_neg <- sum(y_true == 0)
+  AUC <- (sum(rank[y_true == 1]) - n_pos * (n_pos + 1)/2)
+  AUC <- AUC / n_pos  
+  AUC <- AUC / n_neg  
+  return(AUC)
 }
